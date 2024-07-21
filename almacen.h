c5d1bee35be24e6015e078086f117fb622ff26d5
ever <eber.penaloza10@gmail.com>
2024-07-20 23:03:28 -0700

    se agrego un archivo

---
+++ almacen.h
@@ -0,0 +1,86 @@
+#ifndef ALMACEN_H
+#define ALMACEN_H
+
+const int tam=100;
+	int n1=10, n2=10, n3=10, n4=10, n5=10, n6=10;
+	//productos almacenados inicialmente
+	camisetas camiseta[tam]={
+		{"barca", "M", "adidas", 40.00, 2},
+		{"madrid", "S", "adidas", 40.00, 2},
+		{"river", "S", "adidas", 40.00, 2},
+		{"psg", "S", "adidas", 40.00, 2},
+		{"cristal", "S", "adidas", 40.00, 2},
+		{"psg", "M", "adidas", 40.00, 2},
+		{"psv", "S", "adidas", 40.00, 2},
+		{"roma", "S", "adidas", 40.00, 2},
+		{"boca", "M", "adidas", 40.00, 2},
+		{"alianza", "M", "adidas", 40.00, 2}
+	};
+	shorts pantalon_corto[tam]={
+		{"rojo", "M", "adidas", 30.00, 2},
+		{"negro", "S", "adidas", 30.00, 2},
+		{"blanco", "S", "adidas", 30.00, 2},
+		{"naranja", "S", "adidas", 30.00, 2},
+		{"verde", "S", "adidas", 30.00, 2},
+		{"plomo", "M", "adidas", 30.00, 2},
+		{"azul", "S", "adidas", 30.00, 2},
+		{"negro", "L", "adidas", 30.00, 2},
+		{"blanco", "M", "adidas", 30.00, 2},
+		{"rojo", "L", "adidas", 30.00, 2}
+	};
+	zapatillas calzado[tam]={
+		{"rojo", 39, "nike", 300.00, 2},
+		{"negro", 20, "puma", 500.00, 2},
+		{"azul", 40, "puma", 400.00, 2},
+		{"rojo", 37, "umbro", 200.00, 2},
+		{"verde", 42, "adidas", 200.00, 2},
+		{"rosado", 45, "nike", 300.00, 2},
+		{"negro", 43, "adidas", 500.00, 2},
+		{"plomo", 35, "puma", 400.00, 2},
+		{"azul", 30, "nike", 300.00, 2},
+		{"verde", 40, "umbro", 300.00, 2}
+	};
+	canilleras protectores[tam]={
+		{"rojo", "M", "adidas", 30.00, 2},
+		{"verde", "L", "adidas", 30.00, 2},
+		{"azul", "S", "nike", 25.00, 2},
+		{"negro", "M", "nike", 35.00, 2},
+		{"blanco", "L", "nike", 25.00, 2},
+		{"plomo", "S", "adidas", 30.00, 2},
+		{"cafe", "L", "nike", 25.00, 2},
+		{"morado", "M", "adidas", 35.00, 2},
+		{"rosado", "S", "nike", 25.00, 2},
+		{"naranja", "L", "adidas", 30.00, 2}
+	};
+	medias media_larga[tam]={
+		{"rojo", "M", "adidas", 15.00, 2},
+		{"negro", "S", "adidas", 15.00, 2},
+		{"azul", "L", "adidas", 15.00, 2},
+		{"blanco", "M", "adidas", 15.00, 2},
+		{"verde", "L", "adidas", 15.00, 2},
+		{"naranja", "L", "adidas", 15.00, 2},
+		{"plomo", "S", "adidas", 15.00, 2},
+		{"morado", "M", "adidas", 15.00, 2},
+		{"rosado", "M", "adidas", 15.00, 2},
+		{"cafe", "M", "adidas", 15.00, 2}
+	};
+	balones pelota[tam]={
+		{"azul", 1, "adidas", 70.00, 2},
+		{"blanco", 2, "mibalon", 30.00, 2},
+		{"naranja", 3, "nike", 60.00, 2},
+		{"verde", 4, "adidas", 75.00, 2},
+		{"plomo", 3, "mibalon", 30.00, 2},
+		{"blanco", 5, "adidas", 80.00, 2},
+		{"cafe", 2, "nike", 50.00, 2},
+		{"morado", 1, "adidas", 60.00, 2},
+		{"verde", 5, "nike", 65.00, 2},
+		{"azul", 4, "adidas", 60.00, 2}
+	};
+
+
+
+
+
+
+
+#endif
